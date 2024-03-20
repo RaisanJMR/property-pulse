8a3a390b5dbc0f8688cbec365cb387b1bc6feb51
jmrsoftnotions <raisan.jm@softnotions.com>
2024-03-20 07:38:02 +0530

    FEAT: working on properties

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,12 +1,18 @@
-import Link from 'next/link'
-
-const page = () => {
+import properties from '@/properties.json'
+import PropertyCard from '@/components/PropertyCard'
+const PropertiesPage = () => {
   return (
-    <div>
-      <h1 className='text-3xl'>Properties</h1>
-      <Link href="/">Go Home</Link>
-    </div>
+    <section class="px-4 py-6">
+      <div class="container-xl lg:container m-auto px-4 py-6">
+        {properties.length === 0 ? (<p>no properties found</p>) : (<div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {properties.map((p) => (
+            <PropertyCard key={p._id} property={p} />
+          ))}
+        </div>)}
+
+      </div>
+    </section>
   )
 }
 
-export default page
+export default PropertiesPage
