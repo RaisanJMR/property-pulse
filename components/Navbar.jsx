a3334c0ed3069249eaa6bdb6204e8974b3f1c9b5
RaisanJMR <raisanjmr@protonmail.com>
2024-03-31 16:04:51 +0530

    FEAT: auth added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,5 @@
 'use client'
+import { useEffect } from 'react';
 import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 import logo from '@/assets/images/logo-white.png';
@@ -6,14 +7,22 @@ import profileDefault from '@/assets/images/profile.png';
 import Link from 'next/link';
 import { FaGoogle } from 'react-icons/fa'
 import { useState } from 'react';
-
+import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 const Navbar = () => {
+  const { data: session } = useSession()
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
   const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false)
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [providers, setProviders] = useState(false)
 
   const pathname = usePathname()
-
+  useEffect(() => {
+    const setAuthProviders = async () => {
+      const res = await getProviders()
+      setProviders(res)
+    }
+    setAuthProviders()
+  }, [])
+  console.log(providers)
   return (
     <nav className='bg-blue-700 border-b border-blue-500'>
       <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
@@ -71,7 +80,7 @@ const Navbar = () => {
                 >
                   Properties
                 </Link>
-                {isLoggedIn && (<Link
+                {session && (<Link
                   href='/properties/add'
                   className={`${pathname === '/properties/add' ? 'bg-black' : ''} text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
                 >
@@ -82,17 +91,19 @@ const Navbar = () => {
           </div>
 
           {/* <!-- Right Side Menu (Logged Out) --> */}
-          {!isLoggedIn && (<div className='hidden md:block md:ml-6'>
+          {!session && (<div className='hidden md:block md:ml-6'>
             <div className='flex items-center'>
-              <button className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'>
-                <FaGoogle className='mr-2 text-white' />
-                <span>Login or Register</span>
-              </button>
+              {providers && Object.values(providers).map((provider, index) => (
+                <button onClick={() => signIn(provider.id)} key={index} className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'>
+                  <FaGoogle className='mr-2 text-white' />
+                  <span>Login or Register</span>
+                </button>
+              ))}
             </div>
           </div>)}
 
           {/* <!-- Right Side Menu (Logged In) --> */}
-          {isLoggedIn && (
+          {session && (
             <div className='absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0'>
               <Link href='/messages' className='relative group'>
                 <button
@@ -203,15 +214,19 @@ const Navbar = () => {
             >
               Properties
             </Link>
-            {isLoggedIn && (<Link
+            {session && (<Link
               href='/properties/add'
               className={`${pathname === '/properties/add' ? 'bg-black' : ''} text-white block rounded-md px-3 py-2 text-base font-medium`}
             >
               Add Property
             </Link>)}
-            {!isLoggedIn && <button className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4'>
-              <span>Login or Register</span>
-            </button>}
+            {!session && providers && Object.values(providers).map((provider, index) => (
+              <button onClick={() => signIn(provider.id)} key={index} className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'>
+                <FaGoogle className='mr-2 text-white' />
+                <span>Login or Register</span>
+              </button>
+            ))
+            }
           </div>
         </div>
       )}
--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,4 +1,6 @@
 import GoogleProvider from "next-auth/providers/google";
+import connectDB from "@/config/database";
+import User from "@/models/User";
 
 export const authOptions = {
     providers: [
@@ -18,15 +20,30 @@ export const authOptions = {
         // invoked on successfull sign
         async signIn({ profile }) {
             // 1.connect to db
+            await connectDB()
             // 2.user exists
+            const userExists = await User.findOne({ email: profile.email })
+            console.log(userExists)
             // 3.add user to db
+            if (!userExists) {
+                const username = profile.name.slice(0, 20)
+                await User.create({
+                    email: profile.email,
+                    username,
+                    image: profile.picture
+                });
+            }
             // 4.return true to allow sign in
+            return true
         },
         // Modifies section object
-        async session({session}) {
+        async session({ session }) {
             // 1.get user from db
+            const user = await User.findOne({ email: session.user.email })
             // 2.Assign the user id to session
+            session.user.id = user._id
             // 3.return the session
+            return session
         }
     }
 }
